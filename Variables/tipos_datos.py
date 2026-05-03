8bdd47eb4845453e82124a3c73a8686fc6e2dcb8
unknown <carletegon@gmail.com>
2026-05-03 17:10:51 +0200

    Example modify variables & data type

---
+++ Variables/tipos_datos.py
@@ -0,0 +1,26 @@
+# Programa: Ficha secreta del superhéroe
+
+# Tipos de datos básicos
+
+# str (cadena de texto)
+nombre = "Capitana Python"
+poder_principal = "Controlar los bugs con la mente"
+
+# int (numero entero)
+edad = 28
+misiones_completadas = 42
+
+# float (numero decimal)
+nivel_energia = 99.8
+# nivel_energia = "Nuevo valor"
+
+# bool (booleano)
+tiene_capa = True
+
+# NoneType (sin valor)
+compania_actual = None
+
+# Mostramos la información de las variables
+print("=== FICHA SECRETA DEL SUPERHEROE ===")
+print(nombre, poder_principal, edad, misiones_completadas, nivel_energia, tiene_capa, compania_actual)
+print(type(compania_actual))
