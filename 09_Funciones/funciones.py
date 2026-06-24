323146a47d777b5f44d7324db54fe150feef492b
unknown <carletegon@gmail.com>
2026-06-24 17:30:23 +0200

    Init functions python

---
+++ 09_Funciones/funciones.py
@@ -0,0 +1,9 @@
+print('*** Funciones Python ***')
+
+# Definir una función para mandar un saludo
+def saludar_mi_funcion(): # Firma del método
+    # Cuerpo de la función
+    print('Saludos desde una función...')
+
+# Programa principal, llamamos a la función
+saludar_mi_funcion()
