735dbf040020bc33cffaff9091dc3a0bb1c4c310
unknown <carletegon@gmail.com>
2026-06-27 16:08:00 +0200

    Args by name

---
+++ 09_Funciones/argumentos_por_nombres.py
@@ -0,0 +1,16 @@
+print(f'*** Función con argumentos por nombre ***')
+
+def imprimir_persona(nombre, apellido='', edad=0):
+    print(f'Persona: nombre = {nombre}, apellido = {apellido}, edad = {edad}')
+
+# Primero, se llamamos a la función pasando por los argumentos de manera posicional
+imprimir_persona('Manolo', 'García', 32)
+
+# Llamar la función usando argumentos por nombre
+imprimir_persona(nombre='Carlos', apellido='Rojas', edad=28)
+
+# Llamar la función argumentos por nombre, pero intercambiando el orden
+imprimir_persona(edad=28, apellido='Rojas', nombre='Carlos')
+
+# Argumentos con valor por default
+imprimir_persona(nombre='Carlos')
